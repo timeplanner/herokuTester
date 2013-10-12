c10aa7a8aed474b37af87719f8f9dbd88386437b
ranli <ranliran@rdu-OptiPlex.(none)>
2013-10-12 08:46:57 -0400

    parse database data into javascript for svg

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @post = current_user.posts.build if signed_in?
     @city = request.location.city
-
+    gon.posts = Post.limit(10)
   end
 
   def help
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :audio, :comment, :location, :user_id, :video
   belongs_to :user
   validates :user_id, :location, presence: true
-  validates :comment, length: {maximum: 140}
+  validates :comment, presence: true, length: {maximum: 140}
   default_scope order: 'posts.created_at DESC'
 end
